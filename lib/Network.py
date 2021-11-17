ca7b5f86914c05b2eb34064c19522c3f2e09ffc3
tempate <ddiazq28@gmail.com>
2021-11-17 12:26:30 +0100

    Evaluate the validation set after each epoch

--- lib/Network.py
+++ lib/Network.py
@@ -4,6 +4,12 @@ from lib.Chain import Chain
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import f1_score
+
+
+LEARNING_RATE = 0.00001
 
 
 class Network():
@@ -20,23 +26,27 @@ class Network():
             return self.model(vector)
 
     def run(self, data):
-        correct = 0
+        labels = []
+        predictions = []
 
         for sentence, label in data:
             prediction = self.predict(sentence)
             
-            if prediction == self.chain.label_to_key[label]:
-                correct += 1
+            labels.append(self.chain.label_to_key[label])
+            predictions.append(prediction)
 
-        return correct / len(data)
+        return f1_score(labels, predictions)
 
-    def train(self, training_data, validating_data):
-        optimizer = optim.RMSprop(self.model.parameters(), lr=0.0001)
+    def train(self, training_data, validating_data, epochs):
+        optimizer = optim.RMSprop(self.model.parameters(), lr=LEARNING_RATE)
         loss_function = nn.BCELoss()
+
+        losses = []
+        scores = []
 
         # We pass several times over the training data.
         # Usually there are between 5 and 30 epochs.
-        for epoch in range(10):
+        for epoch in range(epochs):
             
             for text, label in training_data:
 
@@ -54,7 +64,19 @@ class Network():
                 loss.backward()
                 optimizer.step()
 
-            self.validate(validating_data, loss_function)
+            loss = self.validate(validating_data, loss_function)
+            score = self.run(validating_data)
+
+            losses.append(loss)
+            scores.append(score)
+
+            print("%d.\tLoss: %.3f\tF1-score: %.3f" % (epoch+1, loss, score))
+
+        plt.plot(list(range(epochs)), losses, 'o', label='Loss')
+        plt.plot(list(range(epochs)), scores, label='F1-score')
+        plt.xlabel("Epoch")
+        plt.legend()
+        plt.show()
 
     def validate(self, data, loss_function):
         loss = 0
@@ -71,4 +93,4 @@ class Network():
             # Compute the loss, gradients, and update the parameters
             loss += loss_function(log_probs, target).item()
 
-        print(loss)
+        return loss / len(data)
